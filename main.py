d7ef4e1eee1000cb9c0db4b4780a15c3c0b58787
Wiinter4 <erikcroitoru@yahoo.com>
2024-06-23 08:54:08 +0100

    Update main.py
    added sliders (testing)

--- main.py
+++ main.py
@@ -6,6 +6,26 @@ from scipy.interpolate import interp1d
 """Inputs are: launch angle from horizontal, strength of gravity g, launch speed u and launch height h.
 Use a fixed increment of time t, the graph must automatically update when inputs are changed."""
 
+from matplotlib.widgets import TextBox
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.2)
+t = np.arange(-2.0, 2.0, 0.001)
+ydata = t ** 2
+initial_text = "t ** 2"
+l, = plt.plot(t, ydata, lw=2)
+
+
+def submit(text):
+    ydata = eval(text)
+    l.set_ydata(ydata)
+    ax.set_ylim(np.min(ydata), np.max(ydata))
+    plt.draw()
+
+axbox = plt.axes([0.1, 0.05, 0.8, 0.075])
+text_box = TextBox(axbox, 'Evaluate', initial=initial_text)
+text_box.on_submit(submit)
+
+
 u = 20 #ms^-1
 g = 9.81 #ms^-2
 angle = 45 #degrees
@@ -24,7 +44,7 @@ x = ux * t
 y = launch_height + uy*t - 1/2 * g * t**2
 x_points = []
 y_points = []
-while y > -10:
+while y > -100:
   x_points.append(x)
   y_points.append(y)
   t += time_step
