b72c038bf737a9ace62936fc658ab3ba0db2c6be
JayceFR <jaycejefferson.vicious@gmail.com>
2024-06-25 08:20:26 +0100

    fix 404 refresh error

--- Projectile/src/index.jsx
+++ Projectile/src/index.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './style.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Nav from './components/nav.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/Projectile'>
+    <HashRouter>
     <Nav/>
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
